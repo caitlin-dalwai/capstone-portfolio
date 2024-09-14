6173ea6550d0fc8009855efdf08c077b6ac25444
caitlin-dalwai <dalwaicaitlin@gmail.com>
2024-09-14 15:39:28 +0200

    facilities

---
+++ frontend-vue/src/views/FacilitiesView.vue
@@ -0,0 +1,53 @@
+<template>
+    <div>
+       <div style="background-image: url('https://caitlin-dalwai.github.io/capstone-imgs/pixlr-image-generator-4f569cb5-d088-4dd0-bcdd-729981db3053%201.png'); width: 1440px; height: 418px; "></div>
+       
+       <div style="margin-bottom: 1%;">
+        <h1 style="font-family: Bungee Inline; font-size: 50px; color: #EB631B; text-shadow: 3px 3px 3px black;">FACILITIES</h1>
+        <h5 style="font-family: Calligraffitti; font-size: 20px;">Take a look inside</h5>
+        <h2 style="font-family: Bungee Inline; font-size: 40px; color: #AAC4E7; margin-top: 2%; text-shadow: 3px 3px 3px black;">RESTAURANT</h2>
+        <div class="container" style="margin-top: 3%;">
+      <div class="row">
+        <div class="col-sm-3">
+          <img src="https://caitlin-dalwai.github.io/capstone-imgs/pixlr-image-generator-415148eb-c6c7-49d8-88b3-2003c75c9890%201.png" alt="">
+        </div>
+        <div class="col-sm-3">
+          <img src="https://caitlin-dalwai.github.io/capstone-imgs/pixlr-image-generator-e122d11f-f186-4c8b-bb26-03e355533b73%201.png" alt="">
+        </div>
+        <div class="col-sm-3">
+          <img src="https://caitlin-dalwai.github.io/capstone-imgs/pixlr-image-generator-2fa93abb-8e35-41f5-ac28-167186c64dc6%201.png" alt="">
+        </div>
+        <div class="col-sm-3">
+          <img src="https://caitlin-dalwai.github.io/capstone-imgs/pixlr-image-generator-6c27de5b-f22c-4c53-90bd-f06a22ebdff6%201.png" alt="">
+        </div>
+      </div>
+    </div>
+    <h2 style="font-family: Bungee Inline; font-size: 40px; color: #AAC4E7; margin-top: 2%; text-shadow: 3px 3px 3px black;">PLAYGROUND</h2>
+        <div class="container" style="margin-top: 3%;">
+      <div class="row">
+        <div class="col-sm-3">
+          <img src="https://caitlin-dalwai.github.io/capstone-imgs/pixlr-image-generator-5b21283b-a59a-4892-ac32-aa37dc18a913%201.png" alt="">
+        </div>
+        <div class="col-sm-3">
+          <img src="https://caitlin-dalwai.github.io/capstone-imgs/pixlr-image-generator-a4813121-dc14-45cc-8f04-8282996498e9%201.png" alt="">
+        </div>
+        <div class="col-sm-3">
+          <img src="https://caitlin-dalwai.github.io/capstone-imgs/pixlr-image-generator-833ee719-3eac-4c09-9fc5-c0508bfe6c27%201.png" alt="">
+        </div>
+        <div class="col-sm-3">
+          <img src="https://caitlin-dalwai.github.io/capstone-imgs/pixlr-image-generator-2fc0b793-897d-43ca-97fe-34540053b6f1%201.png" alt="">
+        </div>
+      </div>
+    </div>
+    
+       </div> 
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style>
+    
+</style>
